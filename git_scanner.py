e6f91f40280a607a27414bac99ab80730f2e5f89
kristina <terekhova@cip.ifi.lmu.de>
2025-10-06 16:24:29 +0200

    Created environment for future requirements file. Started git repo fetching implementation using GitPython. Quick tutorial: https://gitpython.readthedocs.io/en/stable/quickstart.html Docs: https://gitpython.readthedocs.io/en/stable/tutorial.html#tutorial-toplevel

--- git_scanner.py
+++ git_scanner.py
@@ -1,13 +1,18 @@
+from git import Repo
 from huggingface_hub import login
 import argparse
 import json
+import os
 import regex as re
 import sys
 
+# TODO: To create requirements.txt
+# pip freeze > requirements.txt
+# Install dependencies in requirements.txt:
+# pip install -r requirements.txt
+
 # TODO: Make sure that the command line input functions similarly to example provided below
 # git_screener.py --repo <path|url> --n <commits> --out report.json
-
-# TODO: How can --repo recognize both file system paths and URLs? If-else statement?
 
 
 # Initializing arguments that can be recognized by parser
@@ -16,11 +21,28 @@ parser.add_argument("--repo", type=str, help="Git repository name", required=Tru
 parser.add_argument("--n", type=int, help="Number of commits to be scanned", required=True)
 parser.add_argument("--out", type=str, help="Output json file name", default="report.json")
 
-def threat_analysis(repo, n, out):
+def check_if_path(repo_string):
+    # Use RegEx to check if link is a local path (else assumes URL)
+    if os.path.exists(os.path.dirname(repo_string)):
+        return True
+    else:
+        return False
+
+
+
+def threat_analysis(repo_link, n, out):
     # Create a dictionary to store issues found by Llama model (is a defaultdict needed?)
     issue_dict = {}
 
     # TODO: Extract all relevant commits, messages and diffs from repo
+    if check_if_path(repo_link):
+        repo = Repo(repo_link)
+    else:
+        repo = Repo.clone_from(repo_link, os.getcwd())
+
+    # Debug: Print all selected commits to see what we're working with
+    print(repo.head)
+    print(repo.tags)
 
     # TODO: Create a good prompt for finding issues (Llama Guard maybe?)
 
